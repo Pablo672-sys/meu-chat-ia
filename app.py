fd747181bdd11b18f84acafb860b82ba39f1405c
Pablo672-sys <pabloamorim101@gmail.com>
2026-07-29 11:58:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import time
 # Configuração da página
 st.set_page_config(page_title="Minha IA Exclusiva", page_icon="🧠", layout="centered")
 
-st.title("🧠 Meu Portal de IA Super Rápida & Super Pesquisa")
+st.title("🧠 IA Do Pablo! & Imagens")
 
 # 🔐 Puxa a chave da Groq dos Secrets
 try:
@@ -93,7 +93,7 @@ if not st.session_state.logado:
 else:
     st.sidebar.title("Minha Conta")
     st.sidebar.write(f"Usuário: **{st.session_state.usuario_atual.upper()}**")
-    st.sidebar.success("Plano: Groq Ultra Fast + Search 🚀")
+    st.sidebar.success("Plano: Normal 🚀")
     st.sidebar.info("📷 Para criar imagem, use: 'crie uma imagem de [descrição]'")
     st.sidebar.markdown("---")
         
